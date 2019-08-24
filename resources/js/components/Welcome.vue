7cf3b7b8c00f686798a6419c64874bd3b5aabe11
Luke <lukehmu@gmail.com>
2019-08-24 14:57:54 +0100

    initial commit

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex-center position-ref full-height">
+    <p> {{ message }} </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Welcome',
+  data() {
+    return {
+      message: 'HI',
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
